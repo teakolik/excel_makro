2cd8c9430990a8d6b5d003ea57b458d3d0fafe51
teakolik <teakolik@teakolik.com>
2016-11-10 01:18:54 +0300

    Boşluk Sil
    Excel’de A sütununda bulunan tüm değerlerin, sağında veya solunda yer
    alan boşlukları siler.

---
+++ bosluk_sil.vb
@@ -0,0 +1,11 @@
+Sub TEAkolik()
+
+' Makro Boşluk Sil
+' Klavye Kısayolu: Option+Cmd+b
+' @TEAkolik
+' www.teakolik.com
+
+    ExecuteExcel4Macro _
+        "FORMULA.REPLACE("" "","""",2,1,FALSE,FALSE,,FALSE,FALSE,FALSE,FALSE)"
+
+End Sub
